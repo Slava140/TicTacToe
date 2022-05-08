5a69624182e4f22c0f261780caf1178e6f36a4ff
Slava140 <slava20031112@gmail.com>
2022-05-08 20:21:16 +0300

    First commit

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    internal class Game
+    {
+        private static byte _mode;
+
+        internal char winner;
+        internal Game(byte mode = 3)
+        {
+            _mode = mode >= 3 && mode <= 10 ? mode : throw new Exception("Невозможно создать такое игровое поле");
+            GameField = GenerateField();
+        }
+
+        private string[] GenerateField()
+        {
+            string[] result = new string[_mode];
+            for (int i = 0; i < _mode; i++)
+            {
+                result[i] = string.Concat(Enumerable.Repeat('0', _mode));
+            }
+            return result;
+        }
+        internal string[] GameField { get; }
+        internal void SetValue(char value, byte xPos, byte yPos)
+        {
+            if ((value == 'x' || value == 'o') && (xPos < _mode && yPos < _mode))
+            {
+                GameField[yPos] = GameField[yPos].Remove(xPos, 1).Insert(xPos, value.ToString());
+            }
+        }
+        internal void CheckWinner()
+        {
+            if (_mode > 5) { CheckLargeField(GameField, _mode); }
+            else { CheckSmallField(GameField, _mode); }
+        }
+        private void CheckSmallField(string[] field, byte mode)
+        {
+            List<char> leftDiagonal = new List<char>();
+            List<char> RightDiagonal = new List<char>();
+
+            for (int i = 0; i < mode; i++)
+            {
+                if (field.All(str => str[i] == 'x')) { winner = 'x'; }
+                if (field.All(str => str[i] == 'o')) { winner = 'o'; }
+
+                leftDiagonal.Add(field[i][i]);
+                RightDiagonal.Add(field[Math.Abs(i - mode + 1)][i]);
+            }
+
+            if (field.Contains(Repeat('x', mode)) || leftDiagonal.All(c => c == 'x') || RightDiagonal.All(c => c == 'x'))
+            {
+                winner = 'x';
+            }
+            if (field.Contains(Repeat('o', mode)) || leftDiagonal.All(c => c == 'o') || RightDiagonal.All(c => c == 'o'))
+            {
+                winner = 'o';
+            }
+        }
+        private void CheckLargeField(string[] field, byte mode)
+        {
+            for (int kernelPos = 0; kernelPos < (mode - 4); kernelPos++)
+            {
+                string[] kernel = new string[5];
+                for (int pos = 0; pos < 5; pos++)
+                {
+                    kernel[pos] = field[kernelPos + pos].Substring(kernelPos, 5);
+                }
+                CheckSmallField(kernel, 5);
+            }
+        }
+
+        private static string Repeat(char value, int count)
+        {
+            return string.Concat(Enumerable.Repeat(value, count));
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private string _currentTheme = "Dark";
+
+        private readonly byte _mode = 10;
+        private readonly int _cellSize;
+        private readonly double _margin;
+
+        private readonly Game _game;
+        private char _currentElement = 'x';
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Resources = new ResourceDictionary { Source = new Uri($"Themes/{_currentTheme}.xaml", UriKind.Relative) };
+            _game = new Game(_mode);
+            _cellSize = (int)GameCanvas.Width / _mode;
+            _margin = _cellSize * 0.1;
+
+            RefreshField();
+        }
+        private void ThemeChange(object sender, RoutedEventArgs e)
+        {
+            _currentTheme = _currentTheme == "Light" ? "Dark" : "Light";
+            Resources = new ResourceDictionary { Source = new Uri($"Themes/{_currentTheme}.xaml", UriKind.Relative) };
+
+            RefreshField();
+        }
+        private void GameCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(GameCanvas);
+            int mouseX = (int)mousePos.X,
+                mouseY = (int)mousePos.Y;
+
+            byte arrayX = (byte)(mouseX / _cellSize),
+                 arrayY = (byte)(mouseY / _cellSize);
+
+            RefreshField();
+            //if (_game.GetEmptyCells().Contains(arrayX.ToString() + arrayY.ToString()))
+            if (_game.GameField[arrayY][arrayX] == '0')
+            {
+                Canvas elementCanvas = GenerateElement(_currentElement, _cellSize - _margin * 2);
+                elementCanvas.Opacity = 0.3;
+                Canvas.SetLeft(elementCanvas, (arrayX * _cellSize) + _margin);
+                Canvas.SetTop(elementCanvas, (arrayY * _cellSize) + _margin);
+
+                _ = GameCanvas.Children.Add(elementCanvas);
+            }
+        }
+        private void GameCanvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            RefreshField();
+        }
+        private void GameCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePos = e.GetPosition(GameCanvas);
+            int mouseX = (int)mousePos.X,
+                mouseY = (int)mousePos.Y;
+
+            byte arrayX = (byte)(mouseX / _cellSize),
+                 arrayY = (byte)(mouseY / _cellSize);
+
+            if (_game.GameField[arrayY][arrayX] == '0')
+            {
+                _game.SetValue(_currentElement, arrayX, arrayY);
+                _currentElement = _currentElement == 'x' ? 'o' : 'x';
+                _game.CheckWinner();
+            }
+            RefreshField();
+            label.Content = _game.winner;
+            _ = GameCanvas.Children.Add(label);
+        }
+        private void ThemeChangeButton_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void RefreshField()
+        {
+            GameCanvas.Children.Clear();
+            //DrawGrid();
+
+            string[] field = _game.GameField;
+
+            for (byte y = 0; y < _mode; y++)
+            {
+                for (byte x = 0; x < _mode; x++)
+                {
+                    DrawElement(field[y][x], _cellSize - _margin * 2, (x * _cellSize) + _margin, (y * _cellSize) + _margin);
+                }
+            }
+        }
+        private void DrawElement(char element, double size, double x, double y)
+        {
+            Canvas elementCanvas = GenerateElement(element, size);
+            Canvas.SetLeft(elementCanvas, x);
+            Canvas.SetTop(elementCanvas, y);
+
+            GameCanvas.Children.Add(elementCanvas);
+        }
+        private Canvas GenerateElement(char element, double size)
+        {
+            Canvas elementCanvas = new Canvas { Width = size, Height = size};
+
+            if (element == 'x')
+            {
+                Line line1 = new Line { X1 = 0, X2 = size, Y1 = 0, Y2 = size, Style = (Style)Resources["CrossLine"] };
+                Line line2 = new Line { X1 = size, X2 = 0, Y1 = 0, Y2 = size, Style = (Style)Resources["CrossLine"] };
+                elementCanvas.Children.Add(line1);
+                elementCanvas.Children.Add(line2);
+            }
+            else if (element == 'o')
+            {
+                Ellipse circle = new Ellipse { Width = size, Height = size, Style = (Style)Resources["CircleLine"] };
+                elementCanvas.Children.Add(circle);
+            }
+
+            return elementCanvas;
+        }
+        private void DrawGrid()
+        {
+            for (int i = _cellSize; i < _cellSize * _mode; i += _cellSize)
+            {
+                Line verticalLine = new Line { X1 = i, X2 = i, Y1 = 0, Y2 = _cellSize * _mode, Name = "GridLine" };
+                Line horizontalLine = new Line { X1 = 0, X2 = _cellSize * _mode, Y1 = i, Y2 = i, Name = "GridLine" };
+                GameCanvas.Children.Add(verticalLine);
+                GameCanvas.Children.Add(horizontalLine);
+            }
+        }
+
+    }
+}
